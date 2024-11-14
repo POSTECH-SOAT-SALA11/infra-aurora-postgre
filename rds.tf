a74cdf89e793c75e81ae3f829fbc333583f6e666
raulsouza-rm355416 <rm355416@fiap.com.br>
2024-11-14 12:50:58 -0300

    postgres secrets

--- rds.tf
+++ rds.tf
@@ -72,7 +72,7 @@ resource "aws_secretsmanager_secret_version" "db_credentials_version" {
     host           = aws_db_instance.postgresdb.address
     port           = aws_db_instance.postgresdb.port
     db             = aws_db_instance.postgresdb.db_name
-    redis_host     = "my-redis-replication-group-001.axvta7.0001.sae1.cache.amazonaws.com"
+    redis_host     = "my-redis-replication-group-001.p6lc45.0001.sae1.cache.amazonaws.com"
     redis_port     = "6379"
     redis_password = ""
     typeorm        = "postgres://${random_string.username.result}:${random_string.password.result}@${aws_db_instance.postgresdb.address}:${aws_db_instance.postgresdb.port}/${aws_db_instance.postgresdb.db_name}"
